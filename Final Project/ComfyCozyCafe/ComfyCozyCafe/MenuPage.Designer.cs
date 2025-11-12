e1c1116e2919012cdab209c21b9acc4f8aecdd32
audreane20 <81461020+audreane20@users.noreply.github.com>
2025-11-11 23:28:49 -0500

    Add files via upload

---
+++ Final Project/ComfyCozyCafe/ComfyCozyCafe/MenuPage.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ComfyCozyCafe
+{
+    partial class MenuPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drinksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coldDrinksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hotDrinksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.cartToolStripMenuItem,
+            this.checkoutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 66);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drinksToolStripMenuItem,
+            this.pasteriesToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(75, 62);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // cartToolStripMenuItem
+            // 
+            this.cartToolStripMenuItem.Name = "cartToolStripMenuItem";
+            this.cartToolStripMenuItem.Size = new System.Drawing.Size(66, 62);
+            this.cartToolStripMenuItem.Text = "Cart";
+            // 
+            // checkoutToolStripMenuItem
+            // 
+            this.checkoutToolStripMenuItem.Name = "checkoutToolStripMenuItem";
+            this.checkoutToolStripMenuItem.Size = new System.Drawing.Size(111, 62);
+            this.checkoutToolStripMenuItem.Text = "Checkout";
+            // 
+            // drinksToolStripMenuItem
+            // 
+            this.drinksToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.coldDrinksToolStripMenuItem,
+            this.hotDrinksToolStripMenuItem});
+            this.drinksToolStripMenuItem.Name = "drinksToolStripMenuItem";
+            this.drinksToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.drinksToolStripMenuItem.Text = "Drinks";
+            // 
+            // pasteriesToolStripMenuItem
+            // 
+            this.pasteriesToolStripMenuItem.Name = "pasteriesToolStripMenuItem";
+            this.pasteriesToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.pasteriesToolStripMenuItem.Text = "Pasteries";
+            // 
+            // coldDrinksToolStripMenuItem
+            // 
+            this.coldDrinksToolStripMenuItem.Name = "coldDrinksToolStripMenuItem";
+            this.coldDrinksToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.coldDrinksToolStripMenuItem.Text = "Cold Drinks";
+            // 
+            // hotDrinksToolStripMenuItem
+            // 
+            this.hotDrinksToolStripMenuItem.Name = "hotDrinksToolStripMenuItem";
+            this.hotDrinksToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.hotDrinksToolStripMenuItem.Text = "Hot Drinks";
+            // 
+            // MenuPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(186)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MenuPage";
+            this.Text = "MenuPage";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drinksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coldDrinksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hotDrinksToolStripMenuItem;
+    }
+}
---
+++ Final Project/WindowsFormsApp1/MenuPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            menuStrip1.BackColor = System.Drawing.ColorTranslator.FromHtml("#EBBA80");
+        }
+    }
+}

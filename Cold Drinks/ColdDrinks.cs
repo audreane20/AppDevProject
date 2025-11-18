3659486eab15e81751f400395d3f3ce26d17babd
audreane20 <81461020+audreane20@users.noreply.github.com>
2025-11-18 00:13:05 -0500

    Add files via upload

---
+++ Cold Drinks/ColdDrinks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cold_Drinks
+{
+    public partial class ColdDrinks : MenuSetup.MenuSetup
+    {
+        public ColdDrinks()
+        {
+            InitializeComponent();
+        }
+    }
+}

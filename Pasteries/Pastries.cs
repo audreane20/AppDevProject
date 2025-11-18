d6fd98ad166b177d552cdc3470ef288015a76e7b
audreane20 <81461020+audreane20@users.noreply.github.com>
2025-11-18 00:15:23 -0500

    Add files via upload

---
+++ Pasteries/Pastries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pastries
+{
+    public partial class Pastries : MenuSetup.MenuSetup
+    {
+        public Pastries()
+        {
+            InitializeComponent();
+        }
+    }
+}

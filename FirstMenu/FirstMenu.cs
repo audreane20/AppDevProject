e91b1c76336f8ac7d3881f8315501a65ff4f8ec7
audreane20 <81461020+audreane20@users.noreply.github.com>
2025-11-21 14:24:30 -0500

    Add files via upload

--- FirstMenu/FirstMenu.cs
+++ FirstMenu/FirstMenu.cs
@@ -1,49 +1,60 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace FirstMenu
 {
     public partial class FirstMenu : Form
     {
+
+
         public FirstMenu()
         {
             InitializeComponent();
         }
 
-        private void hotDrinkIcon_Click(object sender, EventArgs e)
+        private void hotDrinkIcon_Click_1(object sender, EventArgs e)
         {
             Hot_Drinks.HotDrinks hotDrinksForm = new Hot_Drinks.HotDrinks();
             hotDrinksForm.Show();
+            this.Close();
         }
 
-        private void hotDrinksLabel_Click(object sender, EventArgs e)
+        private void hotDrinksLabel_Click_1(object sender, EventArgs e)
         {
             Hot_Drinks.HotDrinks hotDrinksForm = new Hot_Drinks.HotDrinks();
             hotDrinksForm.Show();
+            this.Close();
         }
 
-        private void coldDrinkIcon_Click(object sender, EventArgs e)
+        private void coldDrinkIcon_Click_1(object sender, EventArgs e)
         {
             Cold_Drinks.ColdDrinks coldDrinksForm = new Cold_Drinks.ColdDrinks();
             coldDrinksForm.Show();
+            this.Close();
         }
 
-        private void coldDrinkLabel_Click(object sender, EventArgs e)
+        private void coldDrinkLabel_Click_1(object sender, EventArgs e)
         {
             Cold_Drinks.ColdDrinks coldDrinksForm = new Cold_Drinks.ColdDrinks();
             coldDrinksForm.Show();
+            this.Close();   
         }
 
-        private void pastriesIcon_Click(object sender, EventArgs e)
+        private void pastriesIcon_Click_1(object sender, EventArgs e)
         {
             Pastries.Pastries pasteriesForm = new Pastries.Pastries();
             pasteriesForm.Show();
+            this.Close();
         }
 
-        private void pastriesLabel_Click(object sender, EventArgs e)
+        private void pastriesLabel_Click_1(object sender, EventArgs e)
         {
             Pastries.Pastries pasteriesForm = new Pastries.Pastries();
             pasteriesForm.Show();
+            this.Close();
         }
+
+        
     }
 }

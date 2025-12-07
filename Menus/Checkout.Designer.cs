674da4104fcf8eb86c649f6ef2ac85630846c170
J0YSICK <jooywho@gmail.com>
2025-12-07 18:08:49 -0500

    Menus 2/3

---
+++ Menus/Checkout.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Menus
+{
+    partial class Checkout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listViewCart = new System.Windows.Forms.ListView();
+            this.colItem = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colQty = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.payButton = new System.Windows.Forms.Button();
+            this.comfyCafeDBDataSet = new Menus.ComfyCafeDBDataSet();
+            this.menuItemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuItemsTableAdapter = new Menus.ComfyCafeDBDataSetTableAdapters.MenuItemsTableAdapter();
+            this.tableAdapterManager = new Menus.ComfyCafeDBDataSetTableAdapters.TableAdapterManager();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.detailsLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.comfyCafeDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuItemsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listViewCart
+            // 
+            this.listViewCart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listViewCart.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colItem,
+            this.colQty,
+            this.colPrice});
+            this.listViewCart.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewCart.FullRowSelect = true;
+            this.listViewCart.GridLines = true;
+            this.listViewCart.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewCart.HideSelection = false;
+            this.listViewCart.Location = new System.Drawing.Point(280, 64);
+            this.listViewCart.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewCart.Name = "listViewCart";
+            this.listViewCart.Size = new System.Drawing.Size(239, 248);
+            this.listViewCart.TabIndex = 1;
+            this.listViewCart.UseCompatibleStateImageBehavior = false;
+            this.listViewCart.View = System.Windows.Forms.View.Details;
+            // 
+            // colItem
+            // 
+            this.colItem.Text = "Item";
+            this.colItem.Width = 133;
+            // 
+            // colQty
+            // 
+            this.colQty.Text = "Qty";
+            this.colQty.Width = 41;
+            // 
+            // colPrice
+            // 
+            this.colPrice.Text = "Price";
+            this.colPrice.Width = 100;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblTotal.Location = new System.Drawing.Point(291, 330);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(195, 41);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total: $0.00";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // payButton
+            // 
+            this.payButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.payButton.BackColor = System.Drawing.Color.White;
+            this.payButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payButton.Location = new System.Drawing.Point(11, 330);
+            this.payButton.Margin = new System.Windows.Forms.Padding(2);
+            this.payButton.Name = "payButton";
+            this.payButton.Size = new System.Drawing.Size(265, 41);
+            this.payButton.TabIndex = 5;
+            this.payButton.Text = "&Pay";
+            this.payButton.UseVisualStyleBackColor = false;
+            // 
+            // comfyCafeDBDataSet
+            // 
+            this.comfyCafeDBDataSet.DataSetName = "ComfyCafeDBDataSet";
+            this.comfyCafeDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // menuItemsBindingSource
+            // 
+            this.menuItemsBindingSource.DataMember = "MenuItems";
+            this.menuItemsBindingSource.DataSource = this.comfyCafeDBDataSet;
+            // 
+            // menuItemsTableAdapter
+            // 
+            this.menuItemsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.MenuItemsTableAdapter = this.menuItemsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Menus.ComfyCafeDBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // detailsLabel
+            // 
+            this.detailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailsLabel.Location = new System.Drawing.Point(12, 64);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(168, 20);
+            this.detailsLabel.TabIndex = 7;
+            this.detailsLabel.Text = "Customer details";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 295);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(118, 295);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "label4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(247, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // Checkout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(187)))), ((int)(((byte)(129)))));
+            this.ClientSize = new System.Drawing.Size(530, 379);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.detailsLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.payButton);
+            this.Controls.Add(this.listViewCart);
+            this.Name = "Checkout";
+            this.Text = "Checkout";
+            this.Load += new System.EventHandler(this.Checkout_Load);
+            this.Controls.SetChildIndex(this.listViewCart, 0);
+            this.Controls.SetChildIndex(this.payButton, 0);
+            this.Controls.SetChildIndex(this.lblTotal, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.detailsLabel, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.textBox3, 0);
+            this.Controls.SetChildIndex(this.radioButton1, 0);
+            this.Controls.SetChildIndex(this.radioButton2, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.textBox4, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.comfyCafeDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuItemsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewCart;
+        private System.Windows.Forms.ColumnHeader colItem;
+        private System.Windows.Forms.ColumnHeader colQty;
+        private System.Windows.Forms.ColumnHeader colPrice;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button payButton;
+        private ComfyCafeDBDataSet comfyCafeDBDataSet;
+        private System.Windows.Forms.BindingSource menuItemsBindingSource;
+        private ComfyCafeDBDataSetTableAdapters.MenuItemsTableAdapter menuItemsTableAdapter;
+        private ComfyCafeDBDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label detailsLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
